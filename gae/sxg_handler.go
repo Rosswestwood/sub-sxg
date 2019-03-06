2351bf9515fd2ffcb663c41f726ba0bcaf932c9c
Tsuyoshi Horo <horo@google.com>
2019-03-06 16:57:03 +0900

    Subresource SXG load demo

--- gae/sxg_handler.go
+++ gae/sxg_handler.go
@@ -103,14 +103,13 @@ func createExchange(params *exchangeParams) (*signedexchange.Exchange, error) {
 	return e, nil
 }
 
-func getHeaderIntegrity(path string, payload []byte, contentType string, ver version.Version, host string) string {
+func getHeaderIntegrity(path string, payload []byte, contentType string, host string) string {
 	contentUrl := "https://" + demoDomainName + path
 	reqHeader := http.Header{}
 	resHeader := http.Header{}
 	resHeader.Add("content-type", contentType)
 
-	e := signedexchange.NewExchange(ver, contentUrl, http.MethodGet, reqHeader, 200, resHeader, []byte(payload))
-
+	e := signedexchange.NewExchange(version.Version1b3, contentUrl, http.MethodGet, reqHeader, 200, resHeader, []byte(payload))
 	if err := e.MiEncodePayload(4096); err != nil {
 		return ""
 	}
@@ -154,6 +153,256 @@ func signedExchangeHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.URL.Path {
 	case "/sxg/hello.sxg":
+		serveExchange(params, q, w)
+	case "/sxg/amptestnocdn.sxg":
+		params.contentUrl = "https://" + demoDomainName + "/amptest/amptestnocdn.html"
+		params.payload = amptestnocdn_payload
+		serveExchange(params, q, w)
+	case "/sxg/amptestnocdn_js_preload.sxg":
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/v0.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/js/v0.js\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/js/v0.js", v0js_payload, "text/javascript", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"preload\";"+
+				"as=\"script\"")
+		params.contentUrl = "https://" + demoDomainName + "/amptest/amptestnocdn.html"
+		params.payload = amptestnocdn_payload
+		serveExchange(params, q, w)
+	case "/sxg/amptestnocdn_js_img_preload.sxg":
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/v0.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/js/v0.js\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/js/v0.js", v0js_payload, "text/javascript", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"preload\";"+
+				"as=\"script\"")
+
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_320_jpg.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_320.jpg\";")
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_640_jpg.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_640.jpg\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_320.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_320.jpg", nikko_320_jpg_payload, "image/jpeg", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_640.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_640.jpg", nikko_640_jpg_payload, "image/jpeg", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_640.jpg>;"+
+				"rel=\"preload\";as=\"image\";"+
+				"imagesrcset=\"https://"+demoDomainName+"/amptest/img/nikko_640.jpg 640w, "+
+				"https://"+demoDomainName+"/amptest/img/nikko_320.jpg 320w\";"+
+				"imagesizes=\"(max-width: 640px) 100vw, 640px\"")
+		params.contentUrl = "https://" + demoDomainName + "/amptest/amptestnocdn.html"
+		params.payload = amptestnocdn_payload
+		serveExchange(params, q, w)
+	case "/sxg/amptestnocdn_js_img_vary_preload.sxg":
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/v0.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/js/v0.js\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/js/v0.js", v0js_payload, "text/javascript", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"preload\";"+
+				"as=\"script\"")
+
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_320_jpg.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/jpeg\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_320.jpg\";")
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_320_webp.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/webp\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_320.jpg\";")
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_640_jpg.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/jpeg\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_640.jpg\";")
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_640_webp.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/webp\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_640.jpg\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_320.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/jpeg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_320.jpg", nikko_320_jpg_payload, "image/jpeg", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_320.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/webp\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_320.jpg", nikko_320_webp_payload, "image/webp", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_640.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/jpeg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_640.jpg", nikko_640_jpg_payload, "image/jpeg", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_640.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"variants=\"accept;image/jpeg,image/webp\";"+
+				"variant-key=\"image/webp\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_640.jpg", nikko_640_webp_payload, "image/webp", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_640.jpg>;"+
+				"rel=\"preload\";as=\"image\";"+
+				"imagesrcset=\"https://"+demoDomainName+"/amptest/img/nikko_640.jpg 640w, "+
+				"https://"+demoDomainName+"/amptest/img/nikko_320.jpg 320w\";"+
+				"imagesizes=\"(max-width: 640px) 100vw, 640px\"")
+		params.contentUrl = "https://" + demoDomainName + "/amptest/amptestnocdn.html"
+		params.payload = amptestnocdn_payload
+		serveExchange(params, q, w)
+
+	case "/sxg/amptestnocdn_js_preload_error.sxg":
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/v0.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/js/v0.js\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/js/v0.js", v0js_payload[1:], "text/javascript", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"preload\";"+
+				"as=\"script\"")
+		params.contentUrl = "https://" + demoDomainName + "/amptest/amptestnocdn.html"
+		params.payload = amptestnocdn_payload
+		serveExchange(params, q, w)
+	case "/sxg/amptestnocdn_js_img_preload_error.sxg":
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/v0.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/js/v0.js\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/js/v0.js", v0js_payload, "text/javascript", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/js/v0.js>;"+
+				"rel=\"preload\";"+
+				"as=\"script\"")
+
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_320_jpg.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_320.jpg\";")
+		w.Header().Add(
+			"link",
+			"<https://"+r.Host+"/sxg/nikko_640_jpg.sxg>;"+
+				"rel=\"alternate\";type=\"application/signed-exchange\";"+
+				"anchor=\"https://"+demoDomainName+"/amptest/img/nikko_640.jpg\";")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_320.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_320.jpg", nikko_320_jpg_payload[1:], "image/jpeg", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_640.jpg>;"+
+				"rel=\"allowed-alt-sxg\";"+
+				"header-integrity=\""+getHeaderIntegrity("/amptest/img/nikko_640.jpg", nikko_640_jpg_payload[1:], "image/jpeg", r.Host)+"\"")
+		params.resHeader.Add(
+			"link",
+			"<https://"+demoDomainName+"/amptest/img/nikko_640.jpg>;"+
+				"rel=\"preload\";as=\"image\";"+
+				"imagesrcset=\"https://"+demoDomainName+"/amptest/img/nikko_640.jpg 640w, "+
+				"https://"+demoDomainName+"/amptest/img/nikko_320.jpg 320w\";"+
+				"imagesizes=\"(max-width: 640px) 100vw, 640px\"")
+		params.contentUrl = "https://" + demoDomainName + "/amptest/amptestnocdn.html"
+		params.payload = amptestnocdn_payload
+		serveExchange(params, q, w)
+	case "/sxg/v0.sxg":
+		params.contentUrl = "https://" + demoDomainName + "/amptest/js/v0.js"
+		params.contentType = "text/javascript"
+		params.payload = v0js_payload
+		w.Header().Add("cache-control", "public, max-age=600")
+		serveExchange(params, q, w)
+	case "/sxg/nikko_320_jpg.sxg":
+		params.contentUrl = "https://" + demoDomainName + "/amptest/img/nikko_320.jpg"
+		params.contentType = "image/jpeg"
+		params.payload = nikko_320_jpg_payload
+		w.Header().Add("cache-control", "public, max-age=600")
+		serveExchange(params, q, w)
+	case "/sxg/nikko_320_webp.sxg":
+		params.contentUrl = "https://" + demoDomainName + "/amptest/img/nikko_320.jpg"
+		params.contentType = "image/webp"
+		params.payload = nikko_320_webp_payload
+		w.Header().Add("cache-control", "public, max-age=600")
+		serveExchange(params, q, w)
+	case "/sxg/nikko_640_jpg.sxg":
+		params.contentUrl = "https://" + demoDomainName + "/amptest/img/nikko_640.jpg"
+		params.contentType = "image/jpeg"
+		params.payload = nikko_640_jpg_payload
+		w.Header().Add("cache-control", "public, max-age=600")
+		serveExchange(params, q, w)
+	case "/sxg/nikko_640_webp.sxg":
+		params.contentUrl = "https://" + demoDomainName + "/amptest/img/nikko_640.jpg"
+		params.contentType = "image/webp"
+		params.payload = nikko_640_webp_payload
+		w.Header().Add("cache-control", "public, max-age=600")
 		serveExchange(params, q, w)
 	default:
 		http.Error(w, "signedExchangeHandler", 404)
